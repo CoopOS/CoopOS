48ad9da763192952808b1576d2b64c00e55eddab
CoopPlayzz <73203995+CoopPlayzz@users.noreply.github.com>
2021-04-03 17:49:17 -0500

    Update install.lua

--- install.lua
+++ install.lua
@@ -4,6 +4,36 @@ local tArgs= {...}
 local delete_Files = false
 local reboot = false
 
+local user = "CoopPlayzz"
+local branch = 1
 
 
 
+for k,v in ipairs(tArgs) do
+  if v == "-d" then
+    delete_Files = true
+  end
+  if v == "-r" then
+    reboot = true
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+wget("http://www.pastebin.com/raw/W5ZkVYSi",".gitget")
+shell.run(".gitget "..user.." Coopplayzz "..branch.." CoopOS")
+formatFS()
+print("Installation completed.")
+if reboot then os.reboot() end
