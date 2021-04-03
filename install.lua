a8a21e17e98b85e66a361f3577aa96037fd584bb
CoopPlayzz <73203995+CoopPlayzz@users.noreply.github.com>
2021-04-03 18:16:59 -0500

    Update install.lua

--- install.lua
+++ install.lua
@@ -2,9 +2,9 @@ local tArgs= {...}
 
 local modetype = beta
 
-local delete_Files = false
+local deleteFiles = false
 local reboot = false
-
+local rebootCountdown = 5
 local user = "CoopPlayzz"
 local branch = modetype
 
@@ -12,7 +12,7 @@ local branch = modetype
 
 for k,v in ipairs(tArgs) do
   if v == "-d" then
-    delete_Files = true
+    deleteFiles = true
   end
   if v == "-r" then
     reboot = true
@@ -24,17 +24,40 @@ end
 
 
 
+print("OS does not install yet it's in dev mode right now. The following things do not do anything but they will soon so don't worry. You have chose:")
+sleep(2)
+print('delete files is '..deleteFiles)
+sleep(.75)
+print('reboot is '..reboot)
 
 
 
-
-
+sleep(6)
 
 
 
 
 wget("http://www.pastebin.com/raw/W5ZkVYSi",".gitget")
-shell.run(".gitget "..user.." CoopOS "..branch.." CoopOSsource")
-formatFS()
-print("Installation completed.")
-if reboot then os.reboot() end
+shell.run(".gitget "..user.." CoopOS "..branch.." CoopOS")
+installOS()
+print("Installation completed.(not relly just a test)")
+
+
+if reboot then
+  print"SYSTEM REBOOTING IN"..rebootCountdown..)
+  sleep(1)
+  rebootCountdown - 1
+  print"SYSTEM REBOOTING IN"..rebootCountdown..)
+  sleep(1)
+  rebootCountdown - 1
+  print"SYSTEM REBOOTING IN"..rebootCountdown..)
+  sleep(1)
+  rebootCountdown - 1
+  print"SYSTEM REBOOTING IN"..rebootCountdown..)
+  sleep(1)
+  rebootCountdown - 1
+  sleep(1)
+  print("REBOOTING")
+  sleep(3)
+  os.reboot()
+end
