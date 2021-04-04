2d45c40630d4ce5915d5f3f58ca03fa98fa51989
CoopPlayzz <73203995+CoopPlayzz@users.noreply.github.com>
2021-04-04 13:29:48 -0500

    Update startup.lua

--- Installer/startup.lua
+++ Installer/startup.lua
@@ -1,2 +1,27 @@
+local versionMode = "beta"
 
-print("test")
+
+print("test completed")
+sleep(7)
+if fs.exists("install.lua") then
+  fs.delete("install.lua")
+end
+
+os.setTextColor(colors.red)
+print('downloading the "installer download"')
+sleep(4)
+print("this will continue in 20 seconds")
+sleep(.75)
+print('right now please hold "Ctrl + T" To quit the install')
+sleep(1)
+print('you will not be able to quit this program in the 20 seconds')
+
+
+
+sleep(20)
+
+os.pullEvent = os.pullEventRaw
+print('installing downloader')
+sleep(2)
+shell.run('wget https://raw.githubusercontent.com/CoopPlayzz/CoopOS/'..versionMode..'/installerdown.lua installerdown.lua')
+print('complete!')
