319983d3b3f3631d6851d72cd6edd6858e9f28d6
Coop <73203995+CoopPlayzz@users.noreply.github.com>
2021-04-11 16:50:01 -0500

    Update gotoMasterPage.js

--- docs/js/gotoMasterPage.js
+++ docs/js/gotoMasterPage.js
@@ -1,3 +1,3 @@
-window.open('https://github.com/CoopPlayzz/CoopOS/tree/master','1618176620142','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;
-
-location.replace("coopplayzz.github.com/CoopOS")
+window.open('https://github.com/CoopPlayzz/CoopOS/tree/master','1618176620142','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');
+location.replace("coopplayzz.github.com/CoopOS");
+return false;
