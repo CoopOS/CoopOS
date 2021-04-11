73d3bc8bbea1e29690023feb93199d9e3062cfbd
Coop <73203995+CoopPlayzz@users.noreply.github.com>
2021-04-11 16:46:02 -0500

    Update gotoMasterPage.js

--- docs/js/gotoMasterPage.js
+++ docs/js/gotoMasterPage.js
@@ -1,11 +1,3 @@
-alert("Loading...")
-alert("Loading...")
-alert("Loading...")
-alert("Loading...")
-alert("Loading...")
-alert("Loading...")
 window.open('https://github.com/CoopPlayzz/CoopOS/tree/master','1618176620142','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;
-alert("Loading...")
-alert("Loading...")
-alert("Loading...")
+
 location.replace("coopplayzz.github.com/CoopOS")
