26a853de292f4058a20f9fdab742abee6180dc6a
Coop <73203995+CoopPlayzz@users.noreply.github.com>
2021-04-11 16:34:40 -0500

    Create gotoMasterPage.js

---
+++ docs/js/gotoMasterPage.js
@@ -0,0 +1,2 @@
+javascript:void window.open('https://github.com/CoopPlayzz/CoopOS/tree/master','1618176620142','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;
+location.replace("coopplayzz.github.com/CoopOS")
