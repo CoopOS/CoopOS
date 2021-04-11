4584c405feb55616d8ee668f86276ea160c5739e
Coop <73203995+CoopPlayzz@users.noreply.github.com>
2021-04-11 16:09:24 -0500

    Add files via upload

---
+++ docs/index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+<link rel="shortcut icon" href="https://raw.githubusercontent.com/CoopPlayzz/CoopOS/beta/docs/img/favicon.ico">
+<title>CoopOS Web</title>
+
+<link rel="preconnect" href="https://fonts.gstatic.com/">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap">
+
+
+<meta property="og:description" content="Welcome to CoopOS. CoopOS is a OS in a Minecraft Mod called Computer Craft install it today. NOTE: only avaleble with Minecraft Java Edition as this is a mod on 1.12.2." />
+<meta name="url" content="https://coopplayzz.github.io/CoopOS/">
+<meta name="identifier-URL" content="https://coopplayzz.github.io/CoopOS">
+<meta property="og:title" content="CoopOS: Home"/>
+<meta property="og:site_name" content="The CoopOS Web"/>
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="https://coopplayzz.github.io/CoopOS/"/>
+<meta property="og:image" content="https://raw.githubusercontent.com/CoopPlayzz/CoopOS/beta/docs/img/CoopOSLogoInvert.png"/>
+<meta content="#ec1c24" data-react-helmet="true" name="theme-color">
+
+
+
+<?php  //put apple <meta>/<link>  ?>
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta content="yes" name="apple-touch-fullscreen" />
+<meta name="apple-mobile-web-app-status-bar-style" content="white">
+<meta name="apple-mobile-web-app-title" content="CoopOS Web">
+<link rel="apple-touch-icon" type="image/png" href="https://raw.githubusercontent.com/CoopPlayzz/CoopOS/beta/docs/img/CoopOSLogoInvert.png" />
+<link rel="apple-touch-startup-image" type="image/png" href="https://raw.githubusercontent.com/CoopPlayzz/CoopOS/beta/docs/img/CoopOSLogo.png">
+
+
+<style>
+body,html {
+	font-family: 'Montserrat', sans-serif;
+}
+
+</style>
+</head>
+<body>
+<h1>Coming Soon:</h1>
+<h3>Wiki:</h3>
+<p>the <strong>CoopOS</strong> Wiki has already been shutdown as this is replacing this AKA <strong>The CoopOS Web</strong></p>
+
+
+
+
+
+</body>
+</html>
