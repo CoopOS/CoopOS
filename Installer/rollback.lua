a827e9188a90e70cc92d6ee17a5b93c07256650d
Coop <cooperschwarting@icloud.com>
2022-02-25 13:02:54 -0600

    rollback update

--- Installer/rollback.lua
+++ Installer/rollback.lua
@@ -1,7 +1,8 @@
 local termpullEvent = os.pullEvent
 os.pullEvent = os.pullEventRaw
-
-
+shell.run("wget")
+shell.run("clear")
 --start rollback
 term.setCursorPos(17, 15)
 term.write("Rolling back changes")
+term.setCursorPos(19pp, 16)
