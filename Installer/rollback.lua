87ff5bd08a5f72f93340896713c7ff7a75602f62
Coop <cooperschwarting@icloud.com>
2022-02-25 13:27:37 -0600

    rollbackupdate 2 for test

--- Installer/rollback.lua
+++ Installer/rollback.lua
@@ -1,8 +1,24 @@
 local termpullEvent = os.pullEvent
 os.pullEvent = os.pullEventRaw
-shell.run("wget")
 shell.run("clear")
+shell.run("wget https://raw.githubusercontent.com/CoopOS/CoopOS/beta/img/dots.nfp /CoopOS/dots0.nfp")
+shell.run("clear")shell.run("wget https://raw.githubusercontent.com/CoopOS/CoopOS/beta/img/dots1.nfp /CoopOS/dots1.nfp")
+shell.run("clear")shell.run("wget https://raw.githubusercontent.com/CoopOS/CoopOS/beta/img/dots2.nfp /CoopOS/dots2.nfp")
+shell.run("clear")shell.run("wget https://raw.githubusercontent.com/CoopOS/CoopOS/beta/img/dots3.nfp /CoopOS/dots3.nfp")
+shell.run("clear")
+local dotsimage = 0
+function dots()
+    
+    local olddots = dotsimage
+    if dotsimage == 3 then dotsimage = 0 else dotsimage++ end
+    return "/CoopOS/dots" + olddots + ".nfp"
+end
 --start rollback
-term.setCursorPos(17, 15)
-term.write("Rolling back changes")
-term.setCursorPos(19pp, 16)
+--term.setCursorPos(19, 16)
+sleep(1)
+for i=10,1,-1 do 
+    term.setCursorPos(17, 15)
+    paintutils.drawImage(paintutils.loadImage(dots()), 19, 16)
+    sleep(1)
+    shell.run("clear")
+end
